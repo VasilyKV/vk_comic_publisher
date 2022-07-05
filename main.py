f5ab0745e100f1e7f58f6ba1773d2243b9cb1024
VasilyKV <103107192+VasilyKV@users.noreply.github.com>
2022-07-05 13:26:04 +0400

    Замечания к уроку
    -добавлена обработка исключений - ошибки в ответе от сервера
    -добавлена обработка исключений  - отсутствие соединения с сервером
    -добавлена обработка исключений -  отсутствии в ответе сервера запрашиваемого данного

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 import os
 import random
 import requests
+import shutil
 
 from dotenv import load_dotenv
 from url_to_file_utils import download_file, get_file_extension
 
 API_VK_VERSION = 5.131
 
+
 def fetch_xkcd_random_comic(folder):
     comic_folder = 'comics/'
     url = 'https://xkcd.com/info.0.json'
     response = requests.get(url)
     response.raise_for_status()
-    comics_count = response.json().get('num')
+    comics_count = response.json()['num']
     random_comic_number = random.randint(1, comics_count)
 
     url = f'https://xkcd.com/{random_comic_number}/info.0.json'
     response = requests.get(url)
     response.raise_for_status()
     comic = response.json()
-    comic_url = comic.get('img')
-    comic_alt = comic.get('alt')
-    comic_num = comic.get('num')
+    comic_url = comic['img']
+    comic_alt = comic['alt']
+    comic_num = comic['num']
     file_path = f'{folder}{comic_num}{get_file_extension(comic_url)}'
     download_file(file_path, comic_url)
     return file_path, comic_alt
@@ -80,8 +82,14 @@ def main():
         file_path, comic_alt = fetch_xkcd_random_comic(folder)
         owner_photo_id, photo_id = upload_photo_vk(vk_token, file_path)
         post_photo_vk_wall(vk_token, group_id, owner_photo_id, photo_id, comic_alt)
+    except requests.exceptions.HTTPError:
+        print('Ошибка ответа сервера')
+    except requests.exceptions.ConnectionError:
+        print('Нет ответа от сервера')
+    except KeyError:
+        print('В ответе от сервера нет запрашиваемых данных')
     finally:
-        os.remove(file_path)
+        shutil.rmtree(folder, ignore_errors=True)
 
 
 if __name__ == '__main__':
