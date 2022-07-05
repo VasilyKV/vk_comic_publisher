a5d5726c7b0bbf96cc1c240bed50d55935412d44
VasilyKV <103107192+VasilyKV@users.noreply.github.com>
2022-07-05 10:42:21 +0400

    Замечания к уроку
    Замена tab на space

--- url_to_file_utils.py
+++ url_to_file_utils.py
@@ -5,17 +5,16 @@ import requests
 
 
 def download_file(filepath, url):
-	directory = os.path.dirname(filepath)
-	os.makedirs(directory, exist_ok = True)
-	response = requests.get(url)
-	response.raise_for_status()
-	with open(filepath, 'wb') as file:
-		file.write(response.content)
+    directory = os.path.dirname(filepath)
+    os.makedirs(directory, exist_ok=True)
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
 
 
 def get_file_extension(url):
-	url_parsed = urlsplit (url)
-	path_unquoted = unquote(url_parsed.path)
-	file_extension = os.path.splitext(path_unquoted)[1]
-	return file_extension
-	
+    url_parsed = urlsplit(url)
+    path_unquoted = unquote(url_parsed.path)
+    file_extension = os.path.splitext(path_unquoted)[1]
+    return file_extension
